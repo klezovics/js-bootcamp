21df3f71bce125818c90d664b8ca8d38cdc06ac5
klezovich <arthur@trevor.io>
2023-05-16 02:12:45 +0200

    Add some more exercises and unit test code

---
+++ js/datatypes/datatypes.js
@@ -0,0 +1,44 @@
+// Implementation of each function is an exercise for the reader.
+
+function createAndPrintVariableOfEachPrimitiveJsType() {
+}
+
+function createAndPrintVariableOfEachCompoundsJsType() {
+}
+
+function createAndPrintVariableOfEachSpecialJsType() {
+}
+
+function createAndPrintTwoDifferentJsObjectsWithSameProperties() {
+}
+
+function createAndPrintTwoJavascriptArraysOfDifferentTypes() {
+}
+
+function createAndPrintAnArrayOfArrays() {
+}
+
+function createAndPrintArrayOfObjects() {
+}
+
+function createAndPrintObjectSomeOfFieldsOfWhichAreArrays() {
+}
+
+function showDifferenceBetweenEqualityAndIdentity() {
+}
+
+function demonstrateTruthyAndFalsyValues() {
+}
+
+function createAndPrintVariableViaEachPossibleKeyword() {
+ // Explain the difference between them in comments
+}
+
+function showHowToUseTheSetClass() {
+}
+
+function showHowToUseTheMapClass() {
+}
+
+function showHowToUseADate() {
+}
